c21df8f3815b7dc07ac2f91c62ae742e46dc0aa6
Shounak Deshpande <shounak8@gmail.com>
2022-07-16 05:17:12 +0530

    corrected destination folder in download_dataset

--- kagglehelper/setup.py
+++ kagglehelper/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='kagglehelper',
+setup(name='kagglehelper1',
 version='1.0.0',
 description='Browse and Download Kaggle Datasets using the helper functions',
 url='https://github.com/shounak8/custom_packages/tree/master/kagglehelper',
 author='Shounak Deshpande',
 author_email='shounak.python@gmail.com',
 license='MIT',
-packages=['kagglehelper'],
+packages=['kagglehelper1'],
 install_requires=['kaggle', 'pandas'],
 zip_safe=False)
